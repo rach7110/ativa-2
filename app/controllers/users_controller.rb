1bb9a6c447fc4adea7ea98a7a441d5c4f5e95bcf
MakerSquare Student <students@themakersquare.com>
2013-08-06 12:57:26 -0500

    preparing for starting mailer lesson

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   def create
 	  @user = User.new(params[:user])
 	  if @user.save
-      UserMailer.signup_confirmation(@user).deliver
 		  flash[:success] = "Welcome to my awesome app!"
 		  redirect_to user_url(@user.id)
 	  else
@@ -22,15 +21,3 @@ class UsersController < ApplicationController
     @users = User.all
   end
 end
-
-
-
-
- if PotentialAdmin.find_by_email(admin.email) && PotentialAdmin.find_by_generated_number(params[:admin][:generated_number])
-      if generated_number == PotentialAdmin.find_by_email(admin.email).generated_number.to_s && admin.save
-        PotentialAdmin.find_by_email(admin.email).destroy
-        redirect_to sign_in_path
-      else redirect_to admin_new_path(generated_number: generated_number)
-      end
-    else redirect_to admin_new_path(generated_number: generated_number)
-    end
